36d5b0c2e8d633c33d603b2928bb2ce4f709d062
Thanh031298 <tranthanh.031298@gmail.com>
2020-06-03 16:11:08 +0700

    completed demo microservice'

---
+++ authentication/src/main/java/com.smartosc.training/AuthenticationApplication.java
@@ -0,0 +1,16 @@
+package com.smartosc.training;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * authentication
+ *
+ * @author thanhttt
+ * @created_at 03/06/2020 - 4:04 PM
+ */
+@SpringBootApplication
+public class AuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationApplication.class, args);
+    }
+}
