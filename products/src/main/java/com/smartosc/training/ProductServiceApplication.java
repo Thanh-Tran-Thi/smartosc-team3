62d60067f00c6f11cb48f9c24575763a2edc303b
Thanh031298 <tranthanh.031298@gmail.com>
2020-06-03 15:56:12 +0700

    add: dependencies for product service

---
+++ products/src/main/java/com/smartosc/training/ProductServiceApplication.java
@@ -0,0 +1,17 @@
+package com.smartosc.training;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * products
+ *
+ * @author thanhttt
+ * @created_at 03/06/2020 - 3:35 PM
+ */
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+}
